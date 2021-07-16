81f074361e82dd0b4bac9616285d78f2ce943a2a
Aniket <aniketupatel1@gmail.com>
2021-07-16 12:58:09 -0400

    added components from material UI and created app bar fame

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react'
-import './App.css';
+import { 
+  Typography,
+  AppBar, 
+  Card, 
+  CardActions, 
+  CardContent, 
+  ardMedia, 
+  CssBaseline,
+  Grid, 
+  Toolbar, 
+  Container 
+} from '@material-ui/core'; 
+
+import { PhotoCamera } from '@material-ui/icons';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello</h1>
-    
-    </div>
+   <>
+    <CssBaseline/>
+    <AppBar position='relative'>
+      <Toolbar>
+        <PhotoCamera/>
+        <Typography variant='h6'>
+          PhotoAlbum
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <main>
+      <div>
+        <Container maxWidth='sm'>
+          <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+            PhotoAlbum
+          </Typography>
+          <Typography variant='h5' align='center' color='textSecondary' paragraph>
+            hello everyon this is a phot album, imtrying to make this sentetnace as long as possible so we cans eee how it wokrs
+          </Typography>
+        </Container>
+      </div>
+    </main>
+   </>
         
     
   );
